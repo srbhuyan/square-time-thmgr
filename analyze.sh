80002b93b1ec3b3ff93916c1a36ee4189fcdddf8
Soumya Ranjan Bhuyan <sbhuyan@massivelyparallel.com>
2024-07-01 08:20:42 +0000

    Add speedup and freeup

--- analyze.sh
+++ analyze.sh
@@ -2,12 +2,12 @@
 
 usage()
 {
-  echo "Usage: $0 <serial algorithm> <parallel algirithm> <iva> <iva data> <iva data file> <core count file> <time serial analytics file> <time parallel analytics file> <space serial analytics file> <space parallel analytics file>"
+  echo "Usage: $0 <serial algorithm> <parallel algirithm> <iva> <iva data> <iva data file> <core count file> <time serial analytics file> <time parallel analytics file> <space serial analytics file> <space parallel analytics file> <speedup analytics file> <freeup analytics file>"
   exit 1
 }
 
-if [ "$#" -ne 10 ]; then
-    echo "Invalid number of parameters. Expected:10 Passed:$#"
+if [ "$#" -ne 12 ]; then
+    echo "Invalid number of parameters. Expected:12 Passed:$#"
     usage
 fi
 
@@ -21,6 +21,8 @@ time_serial_analytics_file=$7
 time_parallel_analytics_file=$8
 space_serial_analytics_file=$9
 space_parallel_analytics_file=${10}
+speedup_analytics_file=${11}
+freeup_analytics_file=${12}
 
 serial_measurement=serial.csv
 parallel_measurement=parallel.csv
@@ -102,6 +104,20 @@ for i in "${!space_parallel[@]}"; do
   fi
 done
 
+# speedup
+speedup=()
+t_max=${time_parallel[0]}
+for t in "${time_parallel[@]}"; do
+  speedup+=(`echo "scale=2;$t_max/$t" | bc`)
+done
+
+# freeup
+freeup=()
+s_max=${space_parallel[0]}
+for s in "${space_parallel[@]}"; do
+  freeup+=(`echo "scale=2;$s_max/$s" | bc`)
+done
+
 jo -p iva=$(jo name=$iva_name values=$(jo -a ${iva[@]})) \
 measurements=$(jo -a ${time_serial[@]}) > time-serial.json
 jo -p iva=$(jo name=core values=$(jo -a ${core[@]})) \
@@ -110,12 +126,18 @@ jo -p iva=$(jo name=$iva_name values=$(jo -a ${iva[@]})) \
 measurements=$(jo -a ${space_serial[@]}) > space-serial.json
 jo -p iva=$(jo name=core values=$(jo -a ${core[@]})) \
 measurements=$(jo -a ${space_parallel[@]}) > space-parallel.json
+jo -p iva=$(jo name=core values=$(jo -a ${core[@]})) \
+measurements=$(jo -a ${speedup[@]}) > speedup.json
+jo -p iva=$(jo name=core values=$(jo -a ${core[@]})) \
+measurements=$(jo -a ${freeup[@]}) > freeup.json
 
 # curve fitting
 fit.py --in-file time-serial.json --out-file time-serial-fitted.json
 fit.py --in-file time-parallel.json --out-file time-parallel-fitted.json
 fit.py --in-file space-serial.json --out-file space-serial-fitted.json
 fit.py --in-file space-parallel.json --out-file space-parallel-fitted.json
+fit.py --in-file speedup.json --out-file speedup-fitted.json
+fit.py --in-file freeup.json --out-file freeup-fitted.json
 
 # time serial
 jo -p \
@@ -152,4 +174,22 @@ predictions=$(jo data="`jq '.fitted_measurements' space-parallel-fitted.json`" n
 polynomial="`jq '.polynomial' space-parallel-fitted.json`" \
 maxError="`jq '.max_error' space-parallel-fitted.json`" \
 > $space_parallel_analytics_file
+
+# speedup
+jo -p \
+iva=$(jo data=$(jo -a ${core[@]}) name=core unit=count) \
+measurements=$(jo data=$(jo -a ${speedup[@]}) name='Tmax/T' unit='') \
+predictions=$(jo data="`jq '.fitted_measurements' speedup-fitted.json`" name='Tmax/T' unit='') \
+polynomial="`jq '.polynomial' speedup-fitted.json`" \
+maxError="`jq '.max_error' speedup-fitted.json`" \
+> $speedup_analytics_file
+
+# freeup
+jo -p \
+iva=$(jo data=$(jo -a ${core[@]}) name=core unit=count) \
+measurements=$(jo data=$(jo -a ${freeup[@]}) name='Smax/S' unit='') \
+predictions=$(jo data="`jq '.fitted_measurements' freeup-fitted.json`" name='Smax/S' unit='') \
+polynomial="`jq '.polynomial' freeup-fitted.json`" \
+maxError="`jq '.max_error' freeup-fitted.json`" \
+> $freeup_analytics_file
 
